6de3c6d81fe089ff2ecbfbb386ce536318044089
ariel <arielokmi@gmail.com>
2022-10-31 16:23:12 +0200

    get_movie_file_path

--- Tmdbi_Downloader.py
+++ Tmdbi_Downloader.py
@@ -1,16 +1,29 @@
+import requests
 from imdb import Cinemagoer
+from keys import  TMDBI_KEY
 
 class tmdbi_download():
     def __init__(self):
-        pass
-    def __id_mdbi__(self,movie_name):
+        self.max_size = "original"
+        self.IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}'
+    def __id_imdb__(self,movie_name):
         ia = Cinemagoer()
         movies = ia.search_movie(movie_name)
         print(movies[0])
         return movie_name
-    
+    def __get__movie__file_path__(self):
+        format_url = requests.get(self.IMG_PATTERN.format(key=TMDBI_KEY,imdbid='tt0095016'))
+        api_response = format_url.json()
+        posters = api_response['posters']
+        first_poster = posters[0]
+        file_path = first_poster["file_path"]
+        print (file_path)
+        return file_path
+
+
 
 
 new_movie = tmdbi_download()
-new_movie.__id_mdbi__("spiderman")
+new_movie.__id_imdb__("spiderman")
+new_movie.__get__movie__urls__()
 
