dd45d2daaebcaed018504ca6cba565d7e8f5f785
ariel <arielokmi@gmail.com>
2022-10-31 16:40:49 +0200

    create-movie_id

--- Tmdbi_Downloader.py
+++ Tmdbi_Downloader.py
@@ -5,15 +5,16 @@ from keys import  TMDBI_KEY
 class tmdbi_download():
     def __init__(self):
         self.max_size = "original"
+        self.movie_id = ''
         self.IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}'
     def __id_imdb__(self,movie_name):
         ia = Cinemagoer()
         movies = ia.search_movie(movie_name)
-        print(movies[0])
-        return movie_name
+        self.movie_id = "tt"+movies[0].movieID
+        return self.movie_id
     def __get__movie__file_path__(self):
-        format_url = requests.get(self.IMG_PATTERN.format(key=TMDBI_KEY,imdbid='tt0095016'))
-        api_response = format_url.json()
+        api_request = requests.get(self.IMG_PATTERN.format(key=TMDBI_KEY,imdbid=self.movie_id))
+        api_response = api_request.json()
         posters = api_response['posters']
         first_poster = posters[0]
         file_path = first_poster["file_path"]
@@ -22,8 +23,7 @@ class tmdbi_download():
 
 
 
-
 new_movie = tmdbi_download()
 new_movie.__id_imdb__("spiderman")
-new_movie.__get__movie__urls__()
+new_movie.__get__movie__file_path__()
 
